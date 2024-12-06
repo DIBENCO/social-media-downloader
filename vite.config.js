6c08c860098f109bad963b54427f8128a90fd53e
Javier Pacual <103720518+DIBENCO@users.noreply.github.com>
2024-12-06 10:16:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
   server: {
     port: process.env.PORT || 3000,  // Usar el puerto proporcionado por Render o 3000 como fallback
   },
@@ -8,8 +9,6 @@ export default {
     outDir: 'dist', // Definir el directorio de salida
   },
   plugins: [
-    plugins: [react()],
-    // Agregar plugins si es necesario
+    react(), // Agregar el plugin de React
   ],
-};
-
+});
